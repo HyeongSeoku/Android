5af68e797e235e519165442819a387d83b6edada
gudtjr3437 <gudtjr3437@naver.com>
2020-08-24 15:11:58 +0900

    MapGPS

---
+++ mapGps/app/src/main/java/com/example/mapgps/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.mapgps;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.naver.maps.geometry.Latng;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
