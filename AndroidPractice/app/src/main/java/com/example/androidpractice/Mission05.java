fa535dd55f34d1487db229b31b7c7b48cefc8a83
gudtjr3437 <gudtjr3437@naver.com>
2020-09-07 23:34:33 +0900

    Create Mission05.java

---
+++ AndroidPractice/app/src/main/java/com/example/androidpractice/Mission05.java
@@ -0,0 +1,15 @@
+package com.example.androidpractice;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Mission05 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mission05);
+    }
+}
