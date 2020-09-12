ec4238134654111b4dcb7b58fbf1a42f7b132fd7
gudtjr3437 <gudtjr3437@naver.com>
2020-09-12 23:19:01 +0900

    서브메뉴
    2가지의 버튼(메인 메뉴로 ,로그인으로)돌아가기

---
+++ AndroidPractice/app/src/main/java/com/example/androidpractice/Mission08_customerMenu.java
@@ -0,0 +1,43 @@
+package com.example.androidpractice;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Mission08_customerMenu extends AppCompatActivity {
+    public static final int REQUEST_CODE_MENU = 1001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.mission08_submenu01);
+
+        Button backToMenu = (Button)findViewById(R.id.buttonBacktoMenu);
+        backToMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("meesage","result message is OK!");
+
+                setResult(Activity.RESULT_OK, resultIntent);
+                finish();
+
+            }
+        });
+
+        Button backToLogin = (Button)findViewById(R.id.buttonBacktoLogin);
+        backToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Mission08_login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
