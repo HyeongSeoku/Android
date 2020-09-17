cc6445613ac517ea60e8a53d033f2748f8c2e38b
gudtjr3437 <gudtjr3437@naver.com>
2020-09-17 14:35:55 +0900

    Update Mission09_main.java

--- AndroidPractice/app/src/main/java/com/example/androidpractice/Mission09_main.java
+++ AndroidPractice/app/src/main/java/com/example/androidpractice/Mission09_main.java
@@ -1,4 +1,16 @@
 package com.example.androidpractice;
 
-public class Mission09_main {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Mission09_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mission09);
+    }
+
 }
