c9f47fff5619a505a949d0e5e6ab48040f93934b
gudtjr3437 <gudtjr3437@naver.com>
2020-09-22 21:08:52 +0900

    바람개비 회전
    Sensor,Sensor Manager 사용

---
+++ AndroidPractice/app/src/main/java/com/example/androidpractice/Project3_2_1.java
@@ -0,0 +1,71 @@
+package com.example.androidpractice;
+
+import android.animation.ObjectAnimator;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Project3_2_1 extends AppCompatActivity implements SensorEventListener {
+
+    ImageView iv_pineWheel;
+    SensorManager sm ;
+    Sensor sensor_linear;
+    ObjectAnimator object = new ObjectAnimator();
+    double timestemp = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.project3_2_1);
+        iv_pineWheel = (ImageView)findViewById(R.id.iv_pinwheel);
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensor_linear = sm.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sm.unregisterListener(this);
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sm.registerListener(this,sensor_linear,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        switch (event.sensor.getType()){
+            case Sensor.TYPE_LINEAR_ACCELERATION:
+
+            double dt = (event.timestamp - timestemp)/100000000;
+
+            if(dt>1){
+                double magnitude = Math.sqrt(Math.pow(event.values[0],2)+Math.pow(event.values[1],2)+Math.pow(event.values[2],2));
+
+                double degree_start = iv_pineWheel.getRotation();
+                double degree_end = degree_start +magnitude * 1000;
+
+                object.cancel();
+                object = ObjectAnimator.ofFloat(iv_pineWheel,"rotation",(float)degree_start,(float)degree_end);
+                object.setInterpolator(new LinearInterpolator());
+                object.setDuration(1000);
+                object.start();
+
+                timestemp = event.timestamp;
+            }
+            break;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
