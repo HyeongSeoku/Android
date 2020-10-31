20e96d78109af4472857ebb3e71c5cb79506b95e
gudtjr3437 <gudtjr3437@naver.com>
2020-10-31 20:29:12 +0900

    Fragment 익히기

--- SampleFragment/app/src/main/java/org/techtown/fragment/MainActivity.java
+++ SampleFragment/app/src/main/java/org/techtown/fragment/MainActivity.java
@@ -1,14 +1,37 @@
 package org.techtown.fragment;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
+    MainFragment mainFragment;
+    MenuFragment menuFragment;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.mainFragment);
+        menuFragment = new MenuFragment();
+
+    }
+    public void onFragmentChanged(int index){
+        if(index == 0)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, menuFragment).commit();
+        }else if(index == 1)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,mainFragment).commit();
+        }
     }
 }
