78c348e812e2de1330a6d952900726e7d9db40c3
gudtjr3437 <gudtjr3437@naver.com>
2020-12-09 15:34:48 +0900

    메인액티비티(미정)

---
+++ RegistTest/app/src/main/java/org/techtown/registtest/MainActivity.java
@@ -0,0 +1,14 @@
+package org.techtown.registtest;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activty_main);
+    }
+}
