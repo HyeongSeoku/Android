61497de449d980f7c0dee781999d4d86f54a1f99
gudtjr3437 <gudtjr3437@naver.com>
2020-12-09 15:33:29 +0900

    회원가입PHP
    Regist.PHP 와 연동해주는 클래스

---
+++ RegistTest/app/src/main/java/org/techtown/registtest/RegisterRequest.java
@@ -0,0 +1,33 @@
+package org.techtown.registtest;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+
+    //서버 URL 설정 (php 파일 연동)
+    final static private String URL = "http://192.168.219.109/RegistTest/Register.php";
+    private Map<String, String> map;
+    //private Map<String, String>parameter;
+
+    public RegisterRequest(String UserEmail, String UserPwd , String UserName, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("UserEmail",UserEmail);
+        map.put("UserPwd",UserPwd);
+        map.put("UserName",UserName);
+    }
+
+    @Override
+    protected Map<String, String>getParams() throws AuthFailureError {
+        return map;
+    }
+
+
+}
