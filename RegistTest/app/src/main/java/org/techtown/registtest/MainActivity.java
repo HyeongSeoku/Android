3ecdcc0bad6da2671091d5b9b7db364604391796
gudtjr3437 <gudtjr3437@naver.com>
2020-12-10 21:28:45 +0900

    앱바 추가

--- RegistTest/app/src/main/java/org/techtown/registtest/MainActivity.java
+++ RegistTest/app/src/main/java/org/techtown/registtest/MainActivity.java
@@ -1,14 +1,25 @@
 package org.techtown.registtest;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Toolbar myToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.activty_main);
+
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        Intent getintent = getIntent();
+        String UserName = getintent.getStringExtra("UserName");
     }
+
 }
