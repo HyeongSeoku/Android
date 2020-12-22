00baf7207b03bb41560d87cfec5bf95c5d72f45b
gudtjr3437 <gudtjr3437@naver.com>
2020-12-22 20:00:13 +0900

    친구 목록
    1. 클릭시 친구 프로필 뜨도록(onclick 해야함)

--- RegistTest/app/src/main/java/org/techtown/registtest/Fragment1.java
+++ RegistTest/app/src/main/java/org/techtown/registtest/Fragment1.java
@@ -3,16 +3,20 @@ package org.techtown.registtest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-public class Fragment1 extends Fragment {
 
+public class Fragment1 extends Fragment {
+    private ListView mListView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
@@ -27,7 +31,26 @@ public class Fragment1 extends Fragment {
             userName.setText(UserName);
         }
 
+        mListView = (ListView)v.findViewById(R.id.listView1);
+        final View header = getLayoutInflater().inflate(R.layout.listview_header,null,false);
+
+        mListView.addHeaderView(header);
+
+        dataSetting();
+
         return v;
+    }
+
+    //친구 수만큼 리스트 생성
+    private void dataSetting(){
+
+        MyAdapter myAdapter = new MyAdapter();
+
+        for(int i=0; i<10; ++i){
+            myAdapter.addItem(ContextCompat.getDrawable(getActivity().getApplicationContext(),R.drawable.icon),"name"+i,"contents_"+i);
+        }
+
+        mListView.setAdapter(myAdapter);
     }
 
     @Override
@@ -35,6 +58,7 @@ public class Fragment1 extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
     }
+
 
 
 }
